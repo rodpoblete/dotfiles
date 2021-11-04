1b70cf379b8ea6cccb0b9da0e6c9d825e3436933
Rodrigo Poblete <rodrigopoblete.dev@gmail.com>
2021-11-04 12:13:32 -0300

    Script de instalación

---
+++ setup.sh
@@ -0,0 +1,88 @@
+NC='\\033[0m'
+
+throw()
+{
+  COLOR='\\033[0;31m'
+  >&2 echo -e ${COLOR}$1${NC}
+}
+
+print()
+{
+  COLOR='\\033[0;32m'
+  echo -e ${COLOR}$1${NC}
+}
+
+warn()
+{
+  COLOR='\\033[0;33m'
+  echo -e ${COLOR}$1${NC}
+}
+
+confirm_ssh()
+{
+  read -p "Have you added the SSH Key? [y/N] " CONFIRM_SSH
+  if [[ $CONFIRM_SSH -ne "y" || $CONFIRM_SSH -ne "Y" ]]
+  then
+    warn "Please add it to continue."
+    confirm_ssh
+  fi
+}
+
+install()
+{
+  # Instalamos git 
+  sudo apt install git -y
+  print "Git installed"
+
+  # Configuramos SSH y la agregamos a nuestra cuenta de Github
+  warn "Please, enter your GitHub email address and name"
+  read -p "Email address: " EMAIL
+  read -p "Name: " NAME
+  git config --global user.name "$NAME"
+  git config --global user.email "$EMAIL"
+  echo "Generating SSH Key"
+  ssh-keygen -t rsa -b 4096 -C "$EMAIL"
+  eval "$(ssh-agent -s)"
+  ssh-add -K ~/.ssh/id_rsa
+  pbcopy < ~/.ssh/id_rsa.pub
+  warn "We have copied your new SSH Key to your clipboard, please add it to your GitHub account going to https://github.com/settings/keys"
+  sleep 2.5
+  open "https://github.com/settings/keys"
+  confirm_ssh
+  print "Git & GitHub configured"
+
+  # Instalamos Node en la versión 16 && npm
+	cd ~ 
+  sudo apt install curl -y
+	curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh~
+	sudo bash nodesource_setup.sh
+	sleep 2.5
+	sudo apt install nodejs -y
+  print "Node.js installed"
+
+  # Chequeamos la versión de Python instalamos pip, herramientas adicionales y entornos virtuales
+  python3 -V
+  sleep 2.5
+  sudo apt install -y python3-pip
+  sudo apt install build-essential libssl-dev libffi-dev python3-dev -y
+  sudo apt install -y python3-venv
+
+  # Instalamos ZSH && OMYZSH
+  sudo apt install zsh -y
+  zsh --version
+  sleep 2.5
+  chsh -s $(which zsh)
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+  # Instalamos Vim Plugged && Neovim
+  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  sudo apt-get install -y neovim
+}
+
+if [ "$1" = "install" ]
+then
+  install
+else
+  throw "Invalid command, available values are 'install' or 'run'"
+fi
